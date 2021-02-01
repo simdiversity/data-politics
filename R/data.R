d57a5e03d21562add934c3fd07fe4bb9fafb0c73
Mattia Egloff <mattia.egloff@unil.ch>
2021-02-01 10:56:07 +0100

    Updated data and doc

--- R/data.R
+++ R/data.R
@@ -1,216 +1,51 @@
-#' italian_legislator_17 1 D_final
-"italian_legislator_17-1-D_final"
-
-#' italian_legislator_17 1 null_councilors
-"italian_legislator_17-1-null_councilors"
-
-#' italian_legislator_17 1 null_votes
-"italian_legislator_17-1-null_votes"
-
-#' italian_legislator_17 1 scores_matrix
-"italian_legislator_17-1-scores_matrix"
-
-#' italian_legislator_17 1 weight
-"italian_legislator_17-1-weight"
-
-#' italian_legislator_17 1 weighted_vote_disputedness
-"italian_legislator_17-1-weighted_vote_disputedness"
-
-#' italian_legislator_17 2 D_final
-"italian_legislator_17-2-D_final"
-
-#' italian_legislator_17 2 null_councilors
-"italian_legislator_17-2-null_councilors"
-
-#' italian_legislator_17 2 null_votes
-"italian_legislator_17-2-null_votes"
-
-#' italian_legislator_17 2 scores_matrix
-"italian_legislator_17-2-scores_matrix"
-
-#' italian_legislator_17 2 weight
-"italian_legislator_17-2-weight"
-
-#' italian_legislator_17 2 weighted_vote_disputedness
-"italian_legislator_17-2-weighted_vote_disputedness"
-
-#' italian_legislator_17 3 D_final
-"italian_legislator_17-3-D_final"
-
-#' italian_legislator_17 3 null_councilors
-"italian_legislator_17-3-null_councilors"
-
-#' italian_legislator_17 3 null_votes
-"italian_legislator_17-3-null_votes"
-
-#' italian_legislator_17 3 scores_matrix
-"italian_legislator_17-3-scores_matrix"
-
-#' italian_legislator_17 3 weight
-"italian_legislator_17-3-weight"
-
-#' italian_legislator_17 3 weighted_vote_disputedness
-"italian_legislator_17-3-weighted_vote_disputedness"
-
-#' italian_legislator_18 1 D_final
-"italian_legislator_18-1-D_final"
-
-#' italian_legislator_18 1 null_councilors
-"italian_legislator_18-1-null_councilors"
-
-#' italian_legislator_18 1 null_votes
-"italian_legislator_18-1-null_votes"
-
-#' italian_legislator_18 1 scores_matrix
-"italian_legislator_18-1-scores_matrix"
-
-#' italian_legislator_18 1 weight
-"italian_legislator_18-1-weight"
-
-#' italian_legislator_18 1 weighted_vote_disputedness
-"italian_legislator_18-1-weighted_vote_disputedness"
-
-#' italian_legislator_18 2 D_final
-"italian_legislator_18-2-D_final"
-
-#' italian_legislator_18 2 null_councilors
-"italian_legislator_18-2-null_councilors"
-
-#' italian_legislator_18 2 null_votes
-"italian_legislator_18-2-null_votes"
-
-#' italian_legislator_18 2 scores_matrix
-"italian_legislator_18-2-scores_matrix"
-
-#' italian_legislator_18 2 weight
-"italian_legislator_18-2-weight"
-
-#' italian_legislator_18 2 weighted_vote_disputedness
-"italian_legislator_18-2-weighted_vote_disputedness"
-
-#' italian_legislator_18 3 D_final
-"italian_legislator_18-3-D_final"
-
-#' italian_legislator_18 3 null_councilors
-"italian_legislator_18-3-null_councilors"
-
-#' italian_legislator_18 3 null_votes
-"italian_legislator_18-3-null_votes"
-
-#' italian_legislator_18 3 scores_matrix
-"italian_legislator_18-3-scores_matrix"
-
-#' italian_legislator_18 3 weight
-"italian_legislator_18-3-weight"
-
-#' italian_legislator_18 3 weighted_vote_disputedness
-"italian_legislator_18-3-weighted_vote_disputedness"
-
-#' swiss_legislator_49 1 D_final
-"swiss_legislator_49-1-D_final"
-
-#' swiss_legislator_49 1 null_councilors
-"swiss_legislator_49-1-null_councilors"
-
-#' swiss_legislator_49 1 null_votes
-"swiss_legislator_49-1-null_votes"
-
-#' swiss_legislator_49 1 scores_matrix
-"swiss_legislator_49-1-scores_matrix"
-
-#' swiss_legislator_49 1 weight
-"swiss_legislator_49-1-weight"
-
-#' swiss_legislator_49 1 weighted_vote_disputedness
-"swiss_legislator_49-1-weighted_vote_disputedness"
-
-#' swiss_legislator_49 2 D_final
-"swiss_legislator_49-2-D_final"
-
-#' swiss_legislator_49 2 null_councilors
-"swiss_legislator_49-2-null_councilors"
-
-#' swiss_legislator_49 2 null_votes
-"swiss_legislator_49-2-null_votes"
-
-#' swiss_legislator_49 2 scores_matrix
-"swiss_legislator_49-2-scores_matrix"
-
-#' swiss_legislator_49 2 weight
-"swiss_legislator_49-2-weight"
-
-#' swiss_legislator_49 2 weighted_vote_disputedness
-"swiss_legislator_49-2-weighted_vote_disputedness"
-
-#' swiss_legislator_49 3 D_final
-"swiss_legislator_49-3-D_final"
-
-#' swiss_legislator_49 3 null_councilors
-"swiss_legislator_49-3-null_councilors"
-
-#' swiss_legislator_49 3 null_votes
-"swiss_legislator_49-3-null_votes"
-
-#' swiss_legislator_49 3 scores_matrix
-"swiss_legislator_49-3-scores_matrix"
-
-#' swiss_legislator_49 3 weight
-"swiss_legislator_49-3-weight"
-
-#' swiss_legislator_49 3 weighted_vote_disputedness
-"swiss_legislator_49-3-weighted_vote_disputedness"
-
-#' swiss_legislator_50 1 D_final
-"swiss_legislator_50-1-D_final"
-
-#' swiss_legislator_50 1 null_councilors
-"swiss_legislator_50-1-null_councilors"
-
-#' swiss_legislator_50 1 null_votes
-"swiss_legislator_50-1-null_votes"
-
-#' swiss_legislator_50 1 scores_matrix
-"swiss_legislator_50-1-scores_matrix"
-
-#' swiss_legislator_50 1 weight
-"swiss_legislator_50-1-weight"
-
-#' swiss_legislator_50 1 weighted_vote_disputedness
-"swiss_legislator_50-1-weighted_vote_disputedness"
-
-#' swiss_legislator_50 2 D_final
-"swiss_legislator_50-2-D_final"
-
-#' swiss_legislator_50 2 null_councilors
-"swiss_legislator_50-2-null_councilors"
-
-#' swiss_legislator_50 2 null_votes
-"swiss_legislator_50-2-null_votes"
-
-#' swiss_legislator_50 2 scores_matrix
-"swiss_legislator_50-2-scores_matrix"
-
-#' swiss_legislator_50 2 weight
-"swiss_legislator_50-2-weight"
-
-#' swiss_legislator_50 2 weighted_vote_disputedness
-"swiss_legislator_50-2-weighted_vote_disputedness"
-
-#' swiss_legislator_50 3 D_final
-"swiss_legislator_50-3-D_final"
-
-#' swiss_legislator_50 3 null_councilors
-"swiss_legislator_50-3-null_councilors"
-
-#' swiss_legislator_50 3 null_votes
-"swiss_legislator_50-3-null_votes"
-
-#' swiss_legislator_50 3 scores_matrix
-"swiss_legislator_50-3-scores_matrix"
-
-#' swiss_legislator_50 3 weight
-"swiss_legislator_50-3-weight"
-
-#' swiss_legislator_50 3 weighted_vote_disputedness
-"swiss_legislator_50-3-weighted_vote_disputedness"
+#' italian_legislator_17
+#'
+#' @format A list containing tbls and a matrix
+#' \describe{
+#'   \item{members_of_parliment}{tbl}
+#'   \item{voting_items}{tbl}
+#'   \item{polls}{poll matrix}
+#'   \item{poll_codes}{numeric codes used in the polls matrix and their meaning}
+#'   \item{partys}{changes of party names}
+#' }
+#' @source \url{https://dati.camera.it/}
+#'
+"italian_legislator_17"
+
+#' italian_legislator_18
+#'
+#' @format A list containing tbls and a matrix
+#' \describe{
+#'   \item{members_of_parliment}{tbl}
+#'   \item{voting_items}{tbl}
+#'   \item{polls}{poll matrix}
+#'   \item{poll_codes}{numeric codes used in the polls matrix and their meaning}
+#'   \item{partys}{changes of party names}
+#' }
+#' @source \url{https://dati.camera.it/}
+"italian_legislator_18"
+
+#' swiss_legislator_49
+#' @format A list containing tbls and a matrix
+#' \describe{
+#'   \item{members_of_parliment}{tbl}
+#'   \item{voting_items}{tbl}
+#'   \item{polls}{poll matrix}
+#'   \item{poll_codes}{numeric codes used in the polls matrix and their meaning}
+#' }
+#' @source \url{http://ws-old.parlament.ch/}
+#' @source \url{https://www.parlament.ch/de/ratsbetrieb/abstimmungen/abstimmung-nr-xls}
+"swiss_legislator_49"
+
+#' swiss_legislator_50
+#' @format A list containing tbls and a matrix
+#' \describe{
+#'   \item{members_of_parliment}{tbl}
+#'   \item{voting_items}{tbl}
+#'   \item{polls}{poll matrix}
+#'   \item{poll_codes}{numeric codes used in the polls matrix and their meaning}
+#' }
+#' @source \url{http://ws-old.parlament.ch/}
+#' @source \url{https://www.parlament.ch/de/ratsbetrieb/abstimmungen/abstimmung-nr-xls}
+"swiss_legislator_50"
 
